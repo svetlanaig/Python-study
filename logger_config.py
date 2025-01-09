906aca0b4be26467ad3dc5e847016b6544bf21a9
Svetlana Ignatova <Svetlana_ignatova@epam.com>
2025-01-09 17:29:12 +0100

    logger add

---
+++ logger_config.py
@@ -0,0 +1,20 @@
+import logging
+
+def setup_logger():
+    logger = logging.getLogger('pytest_logger')
+    logger.setLevel(logging.DEBUG)  # Установка уровня логгирования
+
+    file_handler = logging.FileHandler('test.log')
+    file_handler.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)  # Пример: выводить в консоль только ошибки
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
